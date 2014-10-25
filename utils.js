7d3aaf09cbfc5402cb4836aa949eb5ff722651cb
peterong <peter@tamesoft.com.sg>
2014-10-25 18:55:50 +0800

    Update utils.js
    added GetURLParameter function

--- utils.js
+++ utils.js
@@ -1,1 +1,13 @@
-test
+//Get URL parameter from URL
+//sParam: this is the GET variables
+function GetURLParameter(sParam){
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	
+	for( var i=0; i <sURLVariables.length; i++){
+		var sParameterName = sURLVariables[i].split('=');
+		if(sParameterName[0]==sParam){
+			return sParameterName[1];	
+		}
+	}
+}
